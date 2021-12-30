7a6ce1dadc845681b1cf007de30a482138c5e467
andreamosca <andrea.mosca.prod@gmail.com>
2021-12-30 15:12:40 +0100

    utilizzo dei Portals

--- src/components/UI/ErrModal.jsx
+++ src/components/UI/ErrModal.jsx
@@ -1,28 +1,36 @@
-import React from "react";
+import React, {Fragment} from "react";
+import {createPortal} from "react-dom";
 
 import styles from "./ErrModal.module.css";
+
 import Card from "./Card";
 import Button from "./Button";
 
-const ErrModal = (props) => {
+const Backdrop = (props) => {
+  return <div className={styles.background} onClick={props.onClose}></div>;
+};
+
+const Modal = (props) => {
   return (
-    <div style={{display: "block"}}>
-      <div className={styles.background} onClick={props.onClose}></div>
-      <Card className={styles.dialog}>
-        <header>
-          <h2>{props.title}</h2>
-        </header>
-        <main>
-          <p>
-            {props.content}
-          </p>
-        </main>
-        <footer className={styles.footer}>
-          <Button onClick={props.onClose}>{props.action}</Button>
-        </footer>
-      </Card>
-    </div>
+    <Card className={styles.dialog}>
+      <header>
+        <h2>{props.title}</h2>
+      </header>
+      <main>
+        <p>{props.content}</p>
+      </main>
+      <footer className={styles.footer}>
+        <Button onClick={props.onClose}>{props.action}</Button>
+      </footer>
+    </Card>
   );
+};
+
+const ErrModal = (props) => {
+  return <Fragment>
+    {createPortal(<Backdrop onClose={props.onClose} />, document.getElementById("portals"))}
+    {createPortal(<Modal title={props.title} content={props.content} action={props.action} onClose={props.onClose} />, document.getElementById("portals"))}
+  </Fragment>;
 };
 
 export default ErrModal;
